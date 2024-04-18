7718877479b225c249150403dca6cc1aaacd5032
chuckhu <shuochenghu@gmail.com>
2024-04-18 16:33:50 +0800

    Ch5_6

---
+++ Ch05/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.ch5_6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener {
+    private float size = 20;
+    private TextView output;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        output = (TextView) findViewById(R.id.lblOutput);
+        output.setTextSize(size);
+        Button btn = (Button) findViewById(R.id.button);
+        Button btn2 = (Button) findViewById(R.id.button2);
+        btn.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.button) {
+            size++;
+        }
+        else {
+            size--;
+        }
+        output.setTextSize(size);
+    }
+}
