b31579015d1db3459d4ce304e7338c583898a5ea
chuckhu <shuochenghu@gmail.com>
2024-06-06 15:52:35 +0800

    Supplement

--- Ch09/MainActivity.java
+++ Ch09/MainActivity.java
@@ -1,44 +1,44 @@
-package com.example.ch9_5;
+package com.example.explicitintentdemo2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int SET_RESULT = 1;
+
     private TextView output;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // 取得TextView元件
+
         output = (TextView) findViewById(R.id.lblOutput);
-    }
-    // Button元件的事件處理
-    public void button_Click(View view) {
-        EditText txtOpd1, txtOpd2;
-        // 取得EditText元件
-        txtOpd1 = (EditText) findViewById(R.id.txtOpd1);
-        txtOpd2 = (EditText) findViewById(R.id.txtOpd2);
-        // 建立Intent物件
-        Intent intent = new Intent(this, OpActivity.class);
-        // 建立傳遞資料的Bundle物件
-        Bundle bundle = new Bundle();
-        bundle.putString("OPERAND01",txtOpd1.getText().toString());
-        bundle.putString("OPERAND02",txtOpd2.getText().toString());
-        intent.putExtras(bundle);  // 加上資料
-        // 啟動活動且有回傳資料
-        startActivityForResult(intent, SET_RESULT);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText txtOpd1, txtOpd2;
+                txtOpd1 = (EditText) findViewById(R.id.txtOpd1);
+                txtOpd2 = (EditText) findViewById(R.id.txtOpd2);
+                Intent intent = new Intent(MainActivity.this, OpActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("OPERAND01",txtOpd1.getText().toString());
+                bundle.putString("OPERAND02",txtOpd2.getText().toString());
+                intent.putExtras(bundle);
+                startActivityForResult(intent, 1);
+            }
+        });
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         switch(requestCode) {
-            case SET_RESULT:
+            case 1:
                 if (resultCode == RESULT_OK) {
                     Bundle bundle = data.getExtras();
                     output.setText("計算結果: " + bundle.getDouble("RESULT"));
--- Ch09/OpActivity.java
+++ Ch09/OpActivity.java
@@ -1,58 +1,70 @@
-package com.example.ch9_5;
+package com.example.explicitintentdemo2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
 
 public class OpActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_op);
-    }
-    // Button元件的事件處理方法
-    public void button2_Click(View view) {
-        int opd1, opd2;
-        double result = 0.0;
-        RadioButton rdbAdd, rdbSubtract, rdbMultiply, rdbDivide;
-        CheckBox chkDivide;
-        // 取得傳遞的資料
-        Bundle bundle = this.getIntent().getExtras();
-        if (bundle == null) return;
-        // 取得運算元
-        opd1 = Integer.parseInt(bundle.getString("OPERAND01"));
-        opd2 = Integer.parseInt(bundle.getString("OPERAND02"));
-        // 取得選取的運算子
-        rdbAdd = (RadioButton) findViewById(R.id.rdbAdd);
-        if (rdbAdd.isChecked()) {
-            result = opd1 + opd2; // 加
-        }
-        rdbSubtract = (RadioButton) findViewById(R.id.rdbSubtract);
-        if (rdbSubtract.isChecked()) {
-            result = opd1 - opd2;  // 減
-        }
-        rdbMultiply = (RadioButton) findViewById(R.id.rdbMultiply);
-        if (rdbMultiply.isChecked()) {
-            result = opd1 * opd2;  // 乘
-        }
-        rdbDivide = (RadioButton) findViewById(R.id.rdbDivide);
-        chkDivide = (CheckBox) findViewById(R.id.chkDivide);
-        if (rdbDivide.isChecked()) {
-            if (chkDivide.isChecked())
-                result = opd1 / opd2;  // 整數除法
-            else
-                result = opd1 / (double) opd2;
-        }
-        Intent rIntent = new Intent();
-        // 建立傳回值
-        Bundle rbundle = new Bundle();
-        rbundle.putDouble("RESULT", result);
-        rIntent.putExtras(rbundle);    // 加上資料
-        setResult(RESULT_OK, rIntent); // 設定傳回
-        finish(); // 結束活動
+        CheckBox chkDivide = (CheckBox) findViewById(R.id.chkDivide);
+        //chkDivide.setEnabled(false);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int opd1, opd2;
+                double result = 0.0;
+                RadioButton rdbAdd, rdbSubtract, rdbMultiply, rdbDivide;
+
+                // 取得傳遞的資料
+                Bundle bundle = OpActivity.this.getIntent().getExtras();
+                if (bundle == null) return;
+                // 取得運算元
+                opd1 = Integer.parseInt(bundle.getString("OPERAND01"));
+                opd2 = Integer.parseInt(bundle.getString("OPERAND02"));
+                // 取得選取的運算子
+                rdbAdd = (RadioButton) findViewById(R.id.rdbAdd);
+                if (rdbAdd.isChecked()) {
+                    //chkDivide.setEnabled(false);
+                    result = opd1 + opd2; // 加
+                }
+                rdbSubtract = (RadioButton) findViewById(R.id.rdbSubtract);
+                if (rdbSubtract.isChecked()) {
+                    //chkDivide.setEnabled(false);
+                    result = opd1 - opd2;  // 減
+                }
+                rdbMultiply = (RadioButton) findViewById(R.id.rdbMultiply);
+                if (rdbMultiply.isChecked()) {
+                    //chkDivide.setEnabled(false);
+                    result = opd1 * opd2;  // 乘
+                }
+                rdbDivide = (RadioButton) findViewById(R.id.rdbDivide);
+                //chkDivide = (CheckBox) findViewById(R.id.chkDivide);
+                if (rdbDivide.isChecked()) {
+                    //chkDivide.setEnabled(true);
+                    if (chkDivide.isChecked())
+                        result = opd1 / opd2;  // 整數除法
+                    else
+                        result = opd1 / (double) opd2;
+                }
+                Intent rIntent = new Intent();
+                // 建立傳回值
+                Bundle rbundle = new Bundle();
+                rbundle.putDouble("RESULT", result);
+                rIntent.putExtras(rbundle);    // 加上資料
+                setResult(RESULT_OK, rIntent); // 設定傳回
+                finish(); // 結束活動
+            }
+        });
     }
 }
